2e2c7e1d6919b65030d9a3f32c118ab24e0a8160
walkerch <changxinhai@stu.pku.edu.cn>
2026-02-25 10:47:34 +0800

    feat: add sitemap integration and enhance SEO metadata in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mengtonghuoyun.cn',
@@ -17,5 +19,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
